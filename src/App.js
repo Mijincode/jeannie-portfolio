2b3f659eea85510aa58ec0a74b0c28ac744f5d31
Mijin <mijindoig@hotmail.com>
2025-05-07 09:50:31 +1000

    add id

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route
           path="/"
           element={
-            <main>
+            <main id="main-wrapper">
               <Home />
               <About />
               <Skills />
