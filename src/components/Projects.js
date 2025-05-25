fa5a6a84f46fbbf488edf0e42c214304a3924af6
Mijin <mijindoig@hotmail.com>
2025-05-25 22:46:59 +1000

    Move project data to ProjectData

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import ProjectCard from "./ProjectCard";
-import chaeumImg from "./images/chaeulBeauty/chaeTwo.png";
-import SerenityImg from "./images/styleSerenity/styleSerenity.png";
-import LmsImg from "./images/LMS/Frame6.png";
-import LaptopHappy from "./images/happyToo/Frame5.png";
+
 import "./css/project.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import projectData from "./ProjectData";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -28,73 +26,13 @@ const Projects = () => {
     });
   }, []);
 
-  const projects = {
-    ChaeulBeauty: {
-      hoverText: "Chaeul-Beauty",
-      img: chaeumImg,
-      webPage: "https://chaeul-mi.com/",
-      description:
-        "A beauty tattoo web application for my client in South Korea to enhance customer engagement and operational efficiency.",
-      link: "/projects/chaeul-beauty",
-    },
-
-    StyleSerenityEcommerce: {
-      hoverText: "Style Serenity",
-      img: SerenityImg,
-      description:
-        "A fashion e-commerce web application designed for a seamless shopping experience.",
-      link: "/projects/style-serenity",
-    },
-
-    StudyPlan: {
-      hoverText: "Learning Management System",
-      img: LmsImg,
-      description:
-        "This is a Learning Management System (LMS) web application for Future Leaders Network, enhancing educational experiences through interactive tools",
-      githubUrl: "https://github.com/Mijincode/StudyPlanApp",
-      link: "/projects/study-plan",
-    },
-
-    HappyToo: {
-      hoverText: "Happy Too",
-      // videoSrc: HappyTooVideo,
-      // img: HappyTooImg,
-      img: LaptopHappy,
-      description:
-        "A Busniess Consulting web application designed for a small and medium business",
-    },
-    // HappinessApp: {
-    //   title: "Happiness Ranking",
-    //   videoSrc: "https://www.youtube.com/embed/U-Vrown8XYM",
-    //   description:
-    //     "A React-based web application for analyzing happiness survey data, enabling users to filter and visualize information across demographics.",
-    //   githubUrl: "https://github.com/Mijincode/HappinessApp",
-    // },
-
-    // FlightRoutes: {
-    //   title: "Flight Routes and Airports",
-    //   videoSrc: "https://www.youtube.com/embed/n7On5x0mXxo",
-    //   description:
-    //     "This interactive platform allows users to explore flight routes and airport information across Australia.",
-    //   githubUrl: "https://github.com/Mijincode/FlightRoutes",
-    // },
-
-    // MovieApp: {
-    //   title: "Movie Search",
-    //   videoSrc: "https://www.youtube.com/embed/kMa4L73EOr8",
-    //   description:
-    //     "A movie search app that features user authentication, movie detail lookups via IMDb, and image upload for movie posters.",
-    //   githubUrl: "https://github.com/Mijincode/MovieApp",
-    // },
-  };
-
   return (
     <section id="projects" ref={sectionRef}>
       <div className="project-container">
         <h2 className="project-heading">Projects</h2>
         <div className="card-container">
-          {Object.keys(projects).map((projectKey) => {
-            const project = projects[projectKey];
+          {Object.keys(projectData).map((projectKey) => {
+            const project = projectData[projectKey];
             return (
               <div className="project-wrapper" key={projectKey}>
                 {project.link ? (
