8aae66876eb868d4e360c6b26aa75b8efd27cc11
Mijin <mijindoig@hotmail.com>
2025-07-07 21:37:00 +1000

    Change my photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import myPhoto from "./images/myPhoto3.jpg";
+import myPhoto from "./images/myPhoto.jpg";
 import "./css/home.css";
 
 gsap.registerPlugin(ScrollTrigger);
