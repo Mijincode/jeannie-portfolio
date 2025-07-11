8cc1d8f348f10e0347267fe91464807be5c7105a
Mijin <mijindoig@hotmail.com>
2025-07-11 12:06:41 +1000

    Update css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Link } from "react-router-dom";
 import ProjectCard from "./ProjectCard";
-
+import projectData from "./ProjectData";
 import "./css/project.css";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import projectData from "./ProjectData";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,16 +12,32 @@ export default function Projects() {
   const sectionRef = useRef();
 
   useEffect(() => {
-    const el = sectionRef.current;
-
     const trigger = ScrollTrigger.create({
-      trigger: el,
+      trigger: sectionRef.current,
       start: "top center",
       end: "bottom center",
-      onEnter: () =>
-        gsap.to("body", { backgroundColor: "#4224D2", duration: 0.5 }),
-      onEnterBack: () =>
-        gsap.to("body", { backgroundColor: "#4224D2", duration: 0.5 }),
+      onEnter: () => {
+        gsap.to("body", {
+          backgroundColor: "#4224D2",
+          color: "#FFFFFF",
+          duration: 0.5,
+        });
+        gsap.to(" h1,h2,p, text", {
+          color: "#FFFFFF",
+          duration: 0.5,
+        });
+      },
+      onEnterBack: () => {
+        gsap.to("body", {
+          backgroundColor: "#4224D2",
+          color: "#FFFFFF",
+          duration: 0.5,
+        });
+        gsap.to("h1, h2, p, .text", {
+          color: "#FFFFFF",
+          duration: 0.5,
+        });
+      },
     });
 
     return () => {
