7624dfe1e6353b16bdedf23a06221bf9a6bbc215
Mijin <mijindoig@hotmail.com>
2025-07-23 13:38:04 +1000

    Delete scrollTrigger

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,10 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React from "react";
 import myPhoto from "./images/myPhoto.jpg";
 import "./css/home.css";
 
-gsap.registerPlugin(ScrollTrigger);
-
 export default function Home() {
-  const sectionRef = useRef();
-
-  useEffect(() => {
-    const scrollTriggerInstance = ScrollTrigger.create({
-      trigger: sectionRef.current,
-      start: "top center",
-      end: "bottom center",
-      onEnter: () => {
-        gsap.to("body", {
-          backgroundColor: "#F2C744",
-          color: "#000000",
-          duration: 0.5,
-        });
-        gsap.to("h1, h2, p, .text", { color: "#000000", duration: 0.5 });
-      },
-      onEnterBack: () => {
-        gsap.to("body", {
-          backgroundColor: "#F2C744",
-          color: "#000000",
-          duration: 0.5,
-        });
-        gsap.to("h1, h2, p, .text", { color: "#000000", duration: 0.5 });
-      },
-    });
-
-    return () => {
-      scrollTriggerInstance.kill();
-    };
-  }, []);
-
   return (
-    <section id="home" className="home" ref={sectionRef}>
-      {/* <section id="home" className="home"> */}
+    <section id="home" className="home">
       <div className="home-container">
         <div className="home-content">
           <div className="text-column">
